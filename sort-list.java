b62d6398911178c0754434c7d26f765c1e2eb9aa
shogunsea <shogunsea@foxmail.com>
2014-09-04 15:24:19 -0400

    update sort-list using constant space solution, i.e. bottom-up merge sort approach.

--- sort-list.java
+++ sort-list.java
@@ -12,72 +12,195 @@
  * }
  */
 public class Solution {
+    // recursion : O(nlogn) time, O(logn) stack allocation. each stack creates O(1)
+    // local variables, so space is also O(logn).
+    
+    // public ListNode sortList(ListNode head) {
+    //     if(head == null || head.next == null){
+    //         return head;
+    //     }
+
+    //     // Use fast and slow pointer to get the middle element. Note that slow is
+    //     // one node ahead of the mid element. Where else this technique is used.? 
+    //     // see reorder-list.java
+        
+    //     // two approach of finding middle element, same idea with
+    //     // minor difference:
+
+    //     // Method A:
+    //     // ListNode fast = head;
+    //     // ListNode slow = head;
+
+    //     // while(fast.next != null && fast.next.next != null){
+    //     //     fast = fast.next.next;
+    //     //     slow = slow.next;
+    //     // }
+
+    //     // Method B:
+    //     ListNode fast = head.next.next;
+    //     ListNode slow = head;
+
+    //     while(fast != null && fast.next != null){
+    //         fast = fast.next.next;
+    //         slow = slow.next;
+    //     }
+
+    //     // recursively sort the right half of the list.
+    //     ListNode right = sortList(slow.next);
+    //     // split the left and right sublists.
+    //     slow.next = null;
+    //     // sort the left half.
+    //     ListNode left = sortList(head);
+    //     // merge the two sublists.
+    //     return merge(left, right);
+    // }
+
+    // public ListNode merge(ListNode left, ListNode right){
+    //     ListNode dummy = new ListNode(Integer.MIN_VALUE);
+    //     ListNode runner = dummy;
+    //     while(left != null && right != null){
+    //         if(left.val < right.val){
+    //             runner.next = left;
+    //             left = left.next;
+    //             runner = runner.next;
+    //         }else{
+    //             runner.next = right;
+    //             right = right.next;
+    //             runner = runner.next;
+    //         }
+    //     }
+
+    //     if(left != null){
+    //         runner.next = left;
+    //     }else if(right != null){
+    //         runner.next = right;
+    //     }
+
+    //     return dummy.next;
+    // }
+
+
+    // iterative solution: the bottom-up fashion of merge sort.
+    // sort the list by k steps each time, k starts from 2, each time increase by 
+    // 2 times, i.e. k = 2, 4, 8, etc. 
     public ListNode sortList(ListNode head) {
         if(head == null || head.next == null){
-        	return head;
+            return head;
         }
 
-        int len = 0;
+        ListNode dummy = new ListNode(Integer.MIN_VALUE);
+        dummy.next = head;
 
-        ListNode runner = head;
+        int step = 2;
+        while(true){
+            int run = 0;
 
-        while(runner != null){
-        	len++;
-        	runner = runner.next;
+            ListNode start = dummy;
+
+            while(start != null && start.next != null){
+                start = sortSubList(start, step);
+                run++;
+            }
+
+            if(run <= 1){
+                break;
+            }
+
+            step *= 2;
         }
 
-        runner = head;
-        int mid = len /2 - 1;
+        return dummy.next;
+    }
+
+    public ListNode sortSubList(ListNode node, int subLen){
+        if(node == null || node.next == null){
+            return null;
+        }
+
+        ListNode leftStart = node.next;
+        ListNode leftEnd = move(leftStart, subLen / 2 - 1);
         
-        for(int i = 0; i < mid; i++){
-            runner = runner.next;
+        // for(int i = 0; i < subLen / 2 - 1; i++){
+        //     leftEnd = leftEnd == null? leftEnd : leftEnd.next; 
+        // }
+
+        if(leftEnd == null){
+            return null;
+        }
+
+        ListNode rightStart = leftEnd.next;
+        leftEnd.next = null;
+        ListNode rightEnd = move(rightStart, subLen / 2 - 1);
+
+        // if(rightStart == null){
+        //     return null;
+        // }
+
+        // for(int j = 0; j < subLen / 2 - 1; j++){
+        //     rightEnd = rightEnd == null? rightEnd : rightEnd.next;
+        // }
+
+        ListNode endNode = rightEnd == null? rightEnd : rightEnd.next;
+        
+        if(endNode != null){
+            rightEnd.next = null;
+        }
+
+        while(leftStart != null || rightStart != null){
+            if(rightStart == null || (leftStart != null && leftStart.val < rightStart.val)){
+                node.next = leftStart;
+                leftStart = leftStart.next;
+                node = node.next;
+            }else{
+                node.next = rightStart;
+                rightStart = rightStart.next;
+                node = node.next;
+            }
+        }
+
+        node.next = endNode;
+
+        return node;
+    }
+
+    public ListNode move(ListNode node, int step){
+        while(node != null && step-- > 0){
+            node = node.next;
         }
         
-        ListNode rest = runner.next;
-        runner.next = null;
-
-        ListNode firstHalf = sortList(head);
-        ListNode secondHalf = sortList(rest);
-
-        ListNode resultHead = null, resultTail = null;
-
-        while(firstHalf != null || secondHalf != null){
-        	// firstHalf won't be null at first call.
-        	if(firstHalf == null ){
-        		resultTail.next = secondHalf;
-        		resultTail = resultTail.next;
-        		secondHalf = secondHalf.next;
-        		continue;
-        	}else if(secondHalf == null){
-        		resultTail.next = firstHalf;
-        		resultTail = resultTail.next;
-        		firstHalf = firstHalf.next;
-        		continue;
-        	}
-
-        	if(firstHalf.val < secondHalf.val){
-        		if(resultHead == null){
-        			resultHead = firstHalf;
-        			resultTail = resultHead;
-        			firstHalf = firstHalf.next;
-        		}else{
-        			resultTail.next = firstHalf;
-        			resultTail = resultTail.next;
-        			firstHalf = firstHalf.next;
-        		}
-        	}else{
-        		if(resultHead == null){
-        			resultHead = secondHalf;
-        			resultTail = resultHead;
-        			secondHalf = secondHalf.next;
-        		}else{
-        			resultTail.next = secondHalf;
-        			resultTail = resultTail.next;
-        			secondHalf = secondHalf.next;
-	        	}
-	        }
-    	}
-    	return resultHead;
-	}
+        return node;
+    }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
