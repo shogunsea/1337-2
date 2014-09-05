e045c788396c06d3416c4d4b7c961561500813a5
shogunsea <shogunsea@foxmail.com>
2014-09-05 11:57:10 -0400

    correct integer min value bug in divide two integer

--- divide-two-integers.java
+++ divide-two-integers.java
@@ -10,6 +10,14 @@ public class Solution {
 
         boolean isNeg = (dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0);
 
+
+        // When divisor == Integer.MIN_VALUE, will cause infinite loop, while?
+        // the binary representation of Integer.MIN_VALUE is : 100000000000...0
+        // the abs value of it doesnt chage, which remains: 100000...000
+        // so when you double the value of it by doing bitwise right shift, it
+        // will become 000.000, which will cause the decimal value of it to 0,
+        // so the first while loop will become infite, causing the TLE error.
+
         long dvd  = Math.abs((long)dividend);
         long dvs = Math.abs((long)divisor);
         // dividend = Math.abs(dividend);
@@ -33,6 +41,10 @@ public class Solution {
             step--;
         }
             
-        return res; 
+        return isNeg?  neg(res) : res; 
+    }
+
+    private int neg(int x){
+        return ~x + 1;
     }
 }
