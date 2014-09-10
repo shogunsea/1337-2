903cc5bfe023b08c475dd4b2a6e1db0247168a62
shogunsea <shogunsea@foxmail.com>
2014-09-10 16:46:52 -0400

    update triangle solution using o(n) space. bottom-up approach

--- triangle.java
+++ triangle.java
@@ -12,36 +12,58 @@
 // Note:
 // Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
 
+
+// Space : O(2n * n) --> O(n2) space.
+
+// public class Solution {
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//         if(triangle == null || triangle.size() == 0) return 0;
+
+//         int level = triangle.size();
+
+//         for(int i = 1; i < level; i++){
+//         	List<Integer> previous = triangle.get(i - 1);
+//         	List<Integer> current = triangle.get(i);
+
+//         	for(int j = 0; j < current.size(); j++){
+//         		if(j == 0){
+//         			current.set(j, previous.get(0) + current.get(j));
+//         		}else if(j == current.size() - 1){
+//         			current.set(j, current.get(j) + previous.get(previous.size() - 1));
+//         		}else{
+// 	        		int left = previous.get(j - 1);
+// 	        		int right = previous.get(j);
+
+// 	        		current.set(j, left > right? right + current.get(j) : left + current.get(j));	
+//         		}
+//         	}
+//         }
+
+//         int min = Integer.MAX_VALUE;
+
+//         for(int i : triangle.get(level - 1)){
+//         	min = min > i? i : min;
+//         }
+
+//         return min;
+//     }
+// }
+
 public class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        if(triangle == null || triangle.size() == 0) return 0;
-
-        int level = triangle.size();
-
-        for(int i = 1; i < level; i++){
-        	List<Integer> previous = triangle.get(i - 1);
-        	List<Integer> current = triangle.get(i);
-
-        	for(int j = 0; j < current.size(); j++){
-        		if(j == 0){
-        			current.set(j, previous.get(0) + current.get(j));
-        		}else if(j == current.size() - 1){
-        			current.set(j, current.get(j) + previous.get(previous.size() - 1));
-        		}else{
-	        		int left = previous.get(j - 1);
-	        		int right = previous.get(j);
-
-	        		current.set(j, left > right? right + current.get(j) : left + current.get(j));	
-        		}
-        	}
+        if(triangle == null || triangle.size() == 0){
+            return 0;
         }
-
-        int min = Integer.MAX_VALUE;
-
-        for(int i : triangle.get(level - 1)){
-        	min = min > i? i : min;
+        
+        int len = triangle.size();
+        List<Integer> min = triangle.get(triangle.size() - 1);
+        
+        for(int i = len - 2; i >= 0; i--){
+            for(int j = 0; j <= i; j++){
+                min.set(j, min.get(j) < min.get(j + 1)? triangle.get(i).get(j) + min.get(j) : triangle.get(i).get(j) + min.get(j + 1)); 
+            }
         }
-
-        return min;
+        
+        return min.get(0);
     }
 }
