c59c2643380414aef43a4a780b62bca999bd0893
shogunsea <shogunsea@foxmail.com>
2014-10-12 18:08:31 -0400

    update copy list with random pointer solution

--- copy-list-with-random-pointer.java
+++ copy-list-with-random-pointer.java
@@ -35,3 +35,63 @@ public class Solution {
     	return map.get(head);
     }
 }
+
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        // solution using constant space
+        // the idea is that: interleaving original list by inserting
+        // copied nodes into it during the first iteration,
+        // then go through the list for second time, update the random
+        // pointer
+        // finally iterate the list for third time to revert back original
+        // list and generate the new copied one.
+        if (head == null) {
+            return head;
+        }
+        
+        RandomListNode runner = head;
+        // insert copied nodes into list
+        while (runner != null) {
+            RandomListNode copied = new RandomListNode(runner.label);
+            RandomListNode next = runner.next;
+            runner.next = copied;
+            copied.next = next;
+            runner = next;
+        }
+        // update random pointer
+        runner = head;
+        RandomListNode copiedRunner = runner.next;
+        while (runner != null) {
+            if (runner.random != null) {
+                copiedRunner.random = runner.random.next;
+            }
+            runner = copiedRunner.next;
+            if (runner != null) {
+                copiedRunner = runner.next;
+            }
+        }
+
+        RandomListNode copiedHead = head.next;
+        runner = head;
+        copiedRunner = copiedHead;
+        // revert back
+        while (runner != null) {
+            runner.next = copiedRunner.next;
+            runner = runner.next;
+            if (runner != null) {
+                copiedRunner.next = runner.next;
+                copiedRunner = copiedRunner.next;
+            }
+        }
+
+        return copiedHead;
+    }
+}
