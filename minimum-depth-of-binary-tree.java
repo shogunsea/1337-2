d2406330a5c0f607ae28fb654b96536f0a5a0ec7
shogunsea <shogunsea@foxmail.com>
2014-10-15 10:05:56 -0400

    update min depth of binary tree solution using iteration;

--- minimum-depth-of-binary-tree.java
+++ minimum-depth-of-binary-tree.java
@@ -67,3 +67,45 @@ public class Solution {
     	}
     }
 }
+
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        int min = 1;
+        int size = 1;
+
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < size; i++){
+                TreeNode node = queue.poll();
+                if (node.left == null && node.right == null) {
+                    return min;
+                }
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            size = queue.size();
+            min++;
+        }
+
+        return min;
+    }
+}
