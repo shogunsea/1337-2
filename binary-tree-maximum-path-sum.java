200e790f7299b24af8e7af9ac3493d9abb4c8d4f
shogunsea <shogunsea@foxmail.com>
2015-03-14 16:44:24 -0400

    update path sum solution

--- binary-tree-maximum-path-sum.java
+++ binary-tree-maximum-path-sum.java
@@ -1,24 +1,3 @@
-// Given a binary tree, find the maximum path sum.
-
-// The path may start and end at any node in the tree.
-
-// For example:
-// Given the below binary tree,
-
-//        1
-//       / \
-//      2   3
-// Return 6.
-
-/**
- * Definition for binary tree
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 public class Solution {
     public int maxPathSum(TreeNode root) {
         if(root == null) return 0;
@@ -31,21 +10,22 @@ public class Solution {
     }
 
     public int dfs(TreeNode node, int[] res){
-    	if(node == null) return 0;
+        if(node == null) return 0;
 
-    	int left = dfs(node.left, res) ;
-    	int right = dfs(node.right, res) ;
+        int left = dfs(node.left, res) ;
+        int right = dfs(node.right, res) ;
 
-    	// currentMax candidates(open path not closed, path should be able to extend): 
-    	//	1. node; 2. node + single child.
-    	int currentMax = Math.max(node.val, left > right? left + node.val : right + node.val);
+        // currentMax candidates(open path not closed, path should be able to extend): 
+        //  1. node; 2. node + single child.
+        int currentMax = Math.max(node.val, left > right? left + node.val : right + node.val);
 
-    	// globalMax candidates:(both open&closed path should be considered):
-    	// 1.stored max; 2.node; 3.node + single child; 4.node + both children.
-    	int globalMax = Math.max(res[0], Math.max(node.val, Math.max(left + node.val + right, left > right? node.val + left : node.val + right)));
+        // globalMax candidates:(both open&closed path should be considered):
+        // 1.stored max; 2.node; 3.node + single child; 4.node + both children.
+    //  int globalMax = Math.max(res[0], Math.max(node.val, Math.max(left + node.val + right, left > right? node.val + left : node.val + right)));
+        int globalMax = Math.max(node.val + left + right, currentMax);
 
-    	res[0] = globalMax;
+        res[0] = globalMax > res[0]? globalMax : res[0];
 
-    	return currentMax;
+        return currentMax;
     }
 }
